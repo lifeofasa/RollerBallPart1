250eb67ecfa1f717b1c96762fbf630938c4db114
lifeofasa <lifeofasaoh@gmail.com>
2017-02-10 16:05:02 -0500

    Add files via upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("a"))
+        {
+            rb.useGravity = false;
+            rb.AddForce(Vector3.up * 500f);
+        }
+    }
+}
